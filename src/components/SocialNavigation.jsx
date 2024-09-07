95d7981f49a246686e09f633951b1ff0ad661768
jonayeds <jonayeeds@gmail.com>
2024-09-07 12:25:07 +0600

    fixed navigation bar

--- src/components/SocialNavigation.jsx
+++ src/components/SocialNavigation.jsx
@@ -1,24 +1,34 @@
+import { Link, useLocation } from "react-router-dom"
 
 const SocialNavigation = () => {
+  const location = useLocation()
   return (
     <div className="absolute h-screen w-max  z-20">
-        <div className="lg:hidden top-9 absolute left-1  px-6 py-2">
+        <div className={`${ location.pathname === '/' ? "lg:hidden top-9 absolute left-1  px-6 py-2" : "lg:hidden top-9 absolute -left-20  px-6 py-2" }`}>
             <div className="flex items-center gap-5 font-body font-extralight uppercase tracking-[5px] text-xs">
-                <i>Linkdin</i>
-                <i>Github</i>
+                <div className={` ${ location.pathname !== '/' ? "relative opacity-0" : "group" }`}>
+                <i className="cursor-pointer">Github</i>
+                <div className={`${ location.pathname !== '/' ? "w-full h-full  absolute top-0" : "" }`}></div>
+                <div className="w-0 group-hover:w-full duration-500 bg-black h-[1px] opacity-60 mx-auto  "></div>
+                </div>
+            <Link className="group" to={location.pathname !== "/" && "/" }>
+               <i className="cursor-pointer">{ location.pathname === "/" ? 'Linkdin': 'Home' }</i>
+               <div className="w-0 group-hover:w-full duration-500 bg-black h-[1px] opacity-60 mx-auto"></div>
+               </Link>
                 <div className="w-12 h-[2px] bg-opacity-80 bg-black"></div>
             </div>
         </div>
-        <div className=" hidden lg:block absolute top-[30%]  -rotate-90  -left-28">
-        <div className="flex items-center gap-8 font-body font-extralight uppercase tracking-[6px] text-xs">
-                <div className="w-20 h-[2px] bg-opacity-80 bg-black"></div>
-               <div className="group">
-               <i className="cursor-pointer">Linkdin</i>
+        <div className={`${ location.pathname === '/' ? "hidden lg:flex absolute top-[30%]  -rotate-90  -left-28" : "hidden lg:flex absolute top-[20%]  -rotate-90  -left-28" }`}>
+        <div className="flex items-center gap-8 font-body font-extralight uppercase tracking-[6px] text-xs ">
+                <div className="w-20 h-[2px] bg-opacity-80 bg-black "></div>
+               <Link className="group" to={location.pathname !== "/" && "/" }>
+               <i className="cursor-pointer">{ location.pathname === "/" ? 'Linkdin': 'Home' }</i>
                <div className="w-0 group-hover:w-full duration-500 bg-black h-[1px] opacity-60 mx-auto"></div>
-               </div>
-                <div className="group">
-                <i className="cursor-pointer ">Github</i>
-                <div className="w-0 group-hover:w-full duration-500 bg-black h-[1px] opacity-60 mx-auto "></div>
+               </Link>
+                <div className={` ${ location.pathname !== '/' ? "relative opacity-0" : "group" }`}>
+                <i className="cursor-pointer">Github</i>
+                <div className={`${ location.pathname !== '/' ? "w-full h-full  absolute top-0" : "" }`}></div>
+                <div className="w-0 group-hover:w-full duration-500 bg-black h-[1px] opacity-60 mx-auto  "></div>
                 </div>
             </div>
         </div>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,7 +24,7 @@ const About = () => {
         </div>
 
       {/* about section content*/}
-      <div className="flex items-center justify-center  lg:gap-36 pt-32 lg:max-w-screen-2xl  mx-auto relative px-5 ">
+      <div className="flex items-center justify-center  lg:gap-20 pt-32 lg:max-w-screen-2xl  mx-auto relative px-5 ">
         <div className="lg:max-w-2xl z-10   ">
         <div className='overflow-hidden'>
         <h1 className=" text-[10rem] lg:text-center font-heading   h-full tracking-[8px] about-header  leading-none">About</h1>
@@ -38,7 +38,7 @@ const About = () => {
       <div className="max-w-screen-xl mx-auto px-5 ">
         <p className='font-body tracking-[2px] font-light lg:mt-10 md:mt-36 mt-10 leading-[28px] '>A passionate and dedicated web developer with a strong background in creating dynamic and responsive websites. With a blend of technical skills, creativity, and a keen eye for design, I transform ideas into engaging and functional digital experiences. My journey into web development began when I started my bachelor degree and started to learn how things work on the internet. This curiosity led me to learn Web Development, where I honed my skills in various web technologies. I have worked on many projects that have challenged me to push the boundaries of what`s possible on the web. Now I`m expert in HTML, CSS, and JavaScript, with extensive experience in technologies like React and Tailwind css. Also backend technologies like express js, Node js and MongoDB as database</p>
         <p className='text-3xl mt-10 uppercase  font-body tracking-[2px]  font-light text-dark'>skills</p>
-        <p className='mt-4 text-lg font-normal opacity-80 leading-[30px] tracking-[2px] text-dark'>
+        <p className='mt-4 pb-6 text-lg font-normal opacity-80 leading-[30px] tracking-[2px] text-dark'>
           HTML and CSS / Javascript / Tailwind CSS / <br /> React js / GSAP Animation / <br /> MongoDB / Node.js and Express.js / Mongoose
         </p>
       </div>
