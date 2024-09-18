b65222e8fe8a8ed423f934decdeaf4875aac58c1
jonayeds <jonayeeds@gmail.com>
2024-09-18 19:57:03 +0600

    added send message system

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -37,7 +37,8 @@ const Contact = () => {
           Swal.fire({
             title: 'Success',
             text: 'Message Sent',
-            icon: 'success',
+            icon: 'success',  
+            confirmButtonColor:"#1A1818",
             confirmButtonText: 'OK'
           })
           .then(result =>{
@@ -74,15 +75,15 @@ const Contact = () => {
           <form ref={form} onSubmit={sendEmail} className="md:px-4 w-full">
             <div>
               <p className="font-body font-light tracking-[1px] text-lg">Name</p>
-            <input name="user_name" type="text" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full"  />
+            <input required name="user_name" type="text" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full"  />
             </div>
             <div className="mt-6">
               <p className="font-body font-light tracking-[1px] text-lg">Email</p>
-            <input name="user_email" type="email" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full"  />
+            <input required name="user_email" type="email" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full"  />
             </div>
             <div className="mt-6">
               <p className="font-body font-light tracking-[1px] text-lg">Your Message</p>
-            <textarea name='message' type="text" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full  h-24"  />
+            <textarea required name='message' type="text" className="bg-light text-dark px-4 py-2 rounded-lg mt-2 outline-none w-full  h-24"  />
             </div>
             <div className=' mt-4 pb-8 md:pb-0 flex justify-center'>
         <button type="submit" className='px-10 relative py-2 border-2 border-[#ece7e1]'><div className='absolute w-full text-center flex justify-center bg-dark h-full font-body font-light text-lg top-0 left-0 items-center  hover:tracking-[3px] duration-500 '>Submit</div>Submit</button>
