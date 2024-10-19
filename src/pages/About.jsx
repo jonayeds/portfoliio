9a3d4cafd9c2d5d38abd72f9132b8e098cec9bb4
jonayeds <jonayeeds@gmail.com>
2024-10-19 14:59:05 +0600

    added education and fixed resume button and home text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -39,20 +39,24 @@ const About = () => {
         <h1 className=" md:text-[10rem] text-8xl  mt-10 lg:mt-0 lg:text-center font-heading   h-full tracking-[8px] about-header  leading-none">About</h1>
         </div>
         <p className="font-body text-3xl font-light  tracking-[2px] leading-[45px] mt-16 lg:mt-0 ">I`m Sajjad Jonayed. A Fullstack web developer, problem solver, creative thinker.</p>
+        <div className=' mt-12   max-w-xl  '>
+        <button onClick={downLoadFile} className='px-10  relative py-2 border-2 border-[#1a1a1a]'><div className='absolute w-full text-center flex justify-center bg-[#ece7e1] h-full font-body font-light text-lg top-0 left-0 items-center  hover:tracking-[3px] duration-700 '>My Resume</div>My Resume</button>
+      </div>
         </div>
         <div className="lg:w-96 lg:h-96 w-[60vw]  absolute lg:static    overflow-hidden rounded-2xl right-5 top-24 ">
           <img src={image} className="w-full  profile" alt="" />
         </div>
       </div>
+      
       <div className="lg:max-w-[80vw] max-w-[95vw] mx-auto px-5 ">
         <p className='font-body tracking-[2px] font-light lg:mt-10 md:mt-36 mt-10 leading-[28px] '>A passionate and dedicated web developer with a strong background in creating dynamic and responsive websites. With a blend of technical skills, creativity, and a keen eye for design, I transform ideas into engaging and functional digital experiences. My journey into web development began when I started my bachelor degree and started to learn how things work on the internet. This curiosity led me to learn Web Development, where I honed my skills in various web technologies. I have worked on many projects that have challenged me to push the boundaries of what`s possible on the web. Now I`m expert in HTML, CSS, and JavaScript, with extensive experience in technologies like React and Tailwind css. Also backend technologies like express js, Node js and MongoDB as database</p>
+        <p className='text-3xl mt-10 uppercase  font-body tracking-[2px]  font-light text-dark'>Education</p>
+        <p className='font-body tracking-[2px] font-light lg:mt-6 md:mt-36 mt-6 leading-[28px] '>Bsc. in Computer Science and Engineering at Southeast University.</p>
         <p className='text-3xl mt-10 uppercase  font-body tracking-[2px]  font-light text-dark'>skills</p>
-        <p className='mt-4 pb-6 text-lg font-normal opacity-80 leading-[30px] tracking-[2px] text-dark'>
+        <p className='mt-4 pb-8 text-lg font-normal opacity-80 leading-[30px] tracking-[2px] text-dark'>
           HTML and CSS / Javascript / Tailwind CSS / <br /> React js / GSAP Animation / <br /> MongoDB / Node.js and Express.js / Mongoose
         </p>
-      <div className='pb-6 mt-2'>
-        <button onClick={downLoadFile} className='px-10 relative py-2 border-2 border-[#1a1a1a]'><div className='absolute w-full text-center flex justify-center bg-[#ece7e1] h-full font-body font-light text-lg top-0 left-0 items-center  hover:tracking-[3px] duration-700 '>My Resume</div>My Resume</button>
-      </div>
+      
       </div>
     </div>
   );
